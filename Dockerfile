b4616c5cbec5c125464c562d941ec17873dce6f0
Nivetha S <nivethasaravanan2580@gmail.com>
2024-08-04 19:37:07 +0530

    changed the base image

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 
-FROM node:14
+FROM node:alpine3.18
 WORKDIR /app
 COPY package*.json ./
 RUN npm install
--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -2,11 +2,11 @@ import jwt from 'jsonwebtoken'
 
 export const verifyToken = (req, res, next) => {
    const token = req.cookies.accessToken
-
+   
    if (!token) {
       return res.status(401).json({ success: false, message: "You are not authorize!" })
    }
-
+   
    // if token is exist then verify the token
    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, user) => {
       if (err) {
